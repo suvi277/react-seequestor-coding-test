6af9a8b43662c3a6730d6980cb50ebb6f0b4ed45
Suvarna <suvarna@kalido.me>
2021-04-25 21:59:15 +0100

    updated build steps in node

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 const routes = require('./routes/routes.js')(app, fs);
 
 const server = app.listen(4000, () => {
